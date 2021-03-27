5a46bd8914fef9555307517070f0572334f778da
Samuel Kitazume <samuel.kitazume@gmail.com>
2021-03-26 21:57:15 -0400

    Add a skeleton for the class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+import { Flow, StateEngineInterface, StateID, UserInput } from "./types";
+
+export class StateEngine implements StateEngineInterface {
+  flow: Flow;
+  
+  setFlow(flow: Flow): void {
+    this.flow = flow;
+    return;
+  }
+
+  run(data: UserInput): StateID {
+    return JSON.stringify(data);
+  }
+}
