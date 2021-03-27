243e6250169a288336af767b6f516d2f059b0e2e
Samuel Kitazume <samuel.kitazume@gmail.com>
2021-03-27 14:58:51 -0400

    Create index file for initial tests

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,45 @@
-import { Flow, StateEngineInterface, StateID, UserInput } from "./types";
+import { compileFlow, getYupAstFromStateSchema } from "./StateEngine";
+import { FieldTypes, Flow } from "./types";
 
-export class StateEngine implements StateEngineInterface {
-  flow: Flow;
-  
-  setFlow(flow: Flow): void {
-    this.flow = flow;
-    return;
-  }
+const testFlow: Flow = {
+  id: "test-flow",
+  initialState: "state-1",
+  states: [{
+    id: "state-1",
+    schema: {
+      nome: {
+        type: FieldTypes.STRING,
+        required: true,
+      }
+    }
+  }, {
+    id: "state-2",
+    schema: {
+      email: {
+        type: FieldTypes.STRING,
+        required: true,
+      }
+    }
+  }, {
+    id: "state-3",
+    schema: {
+      valor: {
+        type: FieldTypes.NUMBER,
+        required: true,
+      }
+    }
+  }],
+  links: [{
+    id: "link-1",
+    from: "state-1",
+    to: "state-2",
+  }, {
+    id: "link-1",
+    from: "state-2",
+    to: "state-3",
+  }]
+};
 
-  run(data: UserInput): StateID {
-    return JSON.stringify(data);
-  }
-}
+testFlow.states.map((state) => getYupAstFromStateSchema(state.schema));
+
+console.log(JSON.stringify(compileFlow(testFlow)));
